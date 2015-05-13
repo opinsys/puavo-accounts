efbb7b3bb09abb4d347e7db4512fec0868c9636e
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-05-13 07:04:01 +0000

    Users resource

---
+++ resources/users.rb
@@ -0,0 +1,15 @@
+module PuavoAccounts
+  class Users < Sinatra::Base
+
+
+    get "/new" do
+      "User form"
+
+    end
+
+    post "/" do
+
+    end
+
+  end
+end
--- root.rb
+++ root.rb
@@ -1,12 +1,16 @@
 require "sinatra/base"
 require "sinatra/json"
 
+require_relative "resources/users"
+
 module PuavoAccounts
   class Root < Sinatra::Base
 
     get "/" do
       "puavo accounts"
     end
+
+    use PuavoAccounts::Users
 
   end
 end
