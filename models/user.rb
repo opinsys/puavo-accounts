503f636a221b9936f682820896dcb68cd8512698
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2015-05-18 10:39:54 +0000

    Save user data to the redis database

---
+++ models/user.rb
@@ -0,0 +1,60 @@
+require 'redis'
+
+module PuavoAccounts
+  class User
+    attr_accessor :data, :uuid, :errors
+
+    EXPIRE = 60*60
+
+    UUID_CHARS = ["a".."z", "A".."Z", "0".."9"].reduce([]) do |memo, range|
+      memo + range.to_a
+    end
+
+    def initialize(user_data = {})
+      @data = user_data
+      @errors = nil
+      @uuid = nil
+    end
+
+    def valid?
+      # FIXME validate request to puavo-rest
+      return true
+    end
+
+    def save
+      # FIXME save request to puavo-rest
+      return true
+    end
+
+    def redis_fetch(uuid)
+      @uuid = uuid
+      redis = redis_connection
+      @data = redis.get(uuid_key)
+    end
+
+    def redis_save
+      redis = redis_connection
+      redis.set(uuid_key, @data.to_json)
+      redis.expire(uuid_key, EXPIRE)
+    end
+
+
+    private
+
+    def uuid_key
+      if @uuid.nil?
+        generate_uuid
+      end
+      return "puavo-accounts:user:#{ @uuid }"
+    end
+
+    def redis_connection
+      Redis.new( :db => 6 )
+    end
+
+    def generate_uuid
+      @uuid = (0...50).map{ UUID_CHARS[rand(UUID_CHARS.size)] }.join
+    end
+
+  end
+end
--- root.rb
+++ root.rb
@@ -1,9 +1,14 @@
+require "json"
 require "sinatra/base"
 require "sinatra/json"
 require "sinatra/r18n"
 
+require_relative "models/user"
+
 module PuavoAccounts
   class Root < Sinatra::Base
+
+
 
     register Sinatra::R18n
 
@@ -12,6 +17,8 @@ module PuavoAccounts
     end
 
     get "/new" do
+      @user = User.new()
+
       @locales = [ "fi_FI",
                    "en_US",
                    "sv_FI",
@@ -20,5 +27,35 @@ module PuavoAccounts
       erb :new
     end
 
+    post "/" do
+
+      @user = User.new(params["user"])
+
+      unless @user.valid?
+        # render form
+      end
+
+      # Save user to redis
+      @user.redis_save
+
+      @user.uuid
+
+      # Send email
+    end
+
+    get "/confirm/:uuid" do
+
+      # Get user from redis
+      @user = User.new
+      @user.redis_fetch(params[:uuid])
+
+      if @user.save
+        # render OK
+        @user.data.inspect
+      else
+        # render fail
+      end
+
+    end
   end
 end
