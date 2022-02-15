f6ddf931097112311d26b1bee624eafd1dcdd909
Jarmo Pietiläinen <jarmo@opinsys.fi>
2022-02-15 08:51:30 +0200

    Remove most of the old website functionality
    Since account creation through the website isn't supported anymore
    (hasn't been supported in well over a year now), this isn't needed
    anymore.

--- root.rb
+++ root.rb
@@ -39,7 +39,6 @@ class PuavoRestWrapper
 end
 
 module PuavoAccounts
-
   # The maximum number of characters in first/last/username, password,
   # email and phone fields
   MAXIMUM_FIELD_LENGTH = 32
@@ -47,14 +46,7 @@ module PuavoAccounts
   $mailer = PuavoAccounts::Mailer.new
 
   class Root < Sinatra::Base
-
     enable :logging
-
-    use( Rack::Session::Cookie,
-         :key => 'puavo-accounts',
-         :path => '/accounts',
-         :expire_after => 86400,
-         :secret => CONFIG["session_secret"] )
 
     register Sinatra::R18n
 
@@ -78,7 +70,6 @@ module PuavoAccounts
       logger.error "    message: #{err.message}"
       logger.error "    backtrace: #{err.backtrace}"
     end
-
 
     # --------------------------------------------------------------------------
     # --------------------------------------------------------------------------
@@ -609,171 +600,9 @@ module PuavoAccounts
     # --------------------------------------------------------------------------
     # --------------------------------------------------------------------------
 
-
-    get "/accounts" do
-      @user = User.new
-
-      erb :register_email
+    # Everything else goes here
+    get "*" do
+      erb :layout
     end
-
-    post "/accounts" do
-      @user = User.new(params["user"])
-
-      email = params["user"]["email"]
-
-      if email.nil? or email.empty?
-        @user.add_error("email", t.errors.invalid_email_address)
-        logger.warn "post without email"
-        return erb :register_email
-      end
-
-      jwt_data = {
-        # Issued At
-        "iat" => Time.now.to_i,
-        "email" => email
-      }
-
-      jwt = JWT.encode(jwt_data, CONFIG["jwt"]["secret"])
-
-      @register_url = "https://#{ CONFIG["puavo-rest"]["organisation_domain"] }/accounts/authenticate/#{ jwt }"
-      logger.info "Registration URL: #{ @register_url }"
-      puts "#"*80
-      puts "Registration URL: #{ @register_url }"
-      puts "#"*80
-
-      body = erb(:register_email_message, :layout => false)
-
-      begin
-        $mailer.send( :to => email,
-                      :subject => t.api.register_email.subject,
-                      :body => body )
-      rescue Net::SMTPSyntaxError, Net::SMTPFatalError, ArgumentError => error
-        logger.error "email sending failed:"
-        logger.error "    address: #{email}"
-        logger.error "    error: #{error}"
-        @user.add_error("email", t.errors.invalid_email_address)
-        return erb :register_email
-      end
-
-      logger.info "Send email to following address: #{ email }, IP-address: #{ request.ip }"
-      redirect to("/accounts/complete?email=#{ email }")
-    end
-
-    get "/accounts/complete" do
-      erb :register_email_complete
-    end
-
-    get "/accounts/authenticate/:jwt" do
-      begin
-        jwt_data = JWT.decode(params[:jwt], CONFIG["jwt"]["secret"])
-      rescue JWT::DecodeError
-        logger.warn "invalid jwt data: #{params[:jwt]}"
-        return erb :error, :locals => { :error => t.errors.invalid_jwt }
-      end
-
-      if (Time.now-60*60*24).to_i > jwt_data.first["iat"].to_i
-        logger.warn "jwt token has expired:"
-        logger.warn "    token: #{params[:jwt]}"
-        logger.warn "    data: #{jwt_data}"
-        return erb :error, :locals => { :error => t.errors.invalid_jwt }
-      end
-
-      session[:email] = jwt_data.first["email"]
-
-
-      redirect "/accounts/user"
-    end
-
-    get "/accounts/user" do
-      @user = User.new()
-
-      unless session[:email]
-        return erb :error, :locals => { :error => t.errors.invalid_jwt }
-      end
-
-      erb :new
-
-    end
-
-    post "/accounts/user" do
-      unless session[:email]
-        logger.warn "Cannot create user: no email in session"
-        return erb :error, :locals => { :error => t.errors.invalid_jwt }
-      end
-
-      @user = User.new(params["user"])
-
-      @user.data["email"] = session["email"]
-
-      if params["user"]["password"] != params["user"]["password_confirmation"]
-        logger.warn "Cannot create user: bad password confirmation"
-        @user.add_error("password_confirmation", t.errors.password_confirmation.does_not_match)
-      end
-
-      save_status = @user.save
-
-      if not @user.email_error?
-        logger.warn "Cannot create user: email not unique"
-        return erb :error, :locals => { :error => t.views.new.email_unique_error(session[:email]) }
-      end
-
-      if not save_status or not @user.errors.empty?
-        return erb :new
-      end
-
-      session.delete(:email)
-
-      logger.warn "User created ok"
-
-      body = erb(:successfully_email_message, :layout => false, :locals => { :user => @user.data })
-
-      $mailer.send( :to =>  @user.data["email"],
-                    :subject => t.api.register_email.subject,
-                    :body => body )
-
-      redirect "/accounts/successfully"
-    end
-
-    get "/accounts/successfully" do
-      erb :successfully
-    end
-
-    helpers do
-
-      def form_field(model, attribute, type)
-        @user = model
-        @attr = attribute
-        @errors = []
-        @type = type
-        @name = model.html_attribute(attribute)
-        @css_id = model.css_id(attribute)
-        @value = model.data[attribute]
-
-        @form_group_class = "form-group"
-
-        @error = nil
-        if @user.errors[attribute]
-          @error = @user.errors[attribute].first
-          @form_group_class += " has-error"
-        end
-
-        erb :form_field
-
-      end
-
-      def text_field(model, attribute, options = {})
-        @name = model.html_attribute(attribute)
-        @css_id = model.css_id(attribute)
-        @value = model.data[attribute]
-
-        erb :text_field
-      end
-
-      def h(text)
-        Rack::Utils.escape_html(text)
-      end
-
-    end
-
   end
 end
